950b4998afdffb2174f13fbcd3d0c4492da6e6f1
stefanvaskovic-FN <stefan.vaskovic@flow.ninja>
2026-08-19 15:57:28 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://www.andela.com',
   output: 'static',
+  trailingSlash: 'never',
+  build: {
+    format: 'file',
+  },
 });
